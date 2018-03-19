b033171f2c5d213eeb8f30e06b3027177caa8393
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-03-19 16:54:30 -0700

    Tests demonstrating that POST request body takes priority over query line.

--- src/__tests__/timeseries.post.queryline.tests.js
+++ src/__tests__/timeseries.post.queryline.tests.js
@@ -6,7 +6,7 @@ const timeseriesServiceBase = 'http://localhost:8001/timeseries-service/api/v1';
 const callRESTService =  rest.wrap(mime, { mime: 'application/json' } );
 
 
-describe("When a values POST uses query line only to request first pixel of each band in 5x5x5 data cube", async () => {
+describe("When a values POST request uses the query line only to request first pixel of each band in 5x5x5 data cube", async () => {
     
 	var response;
 	
@@ -45,6 +45,102 @@ describe("When a values POST uses query line only to request first pixel of each
     
     it ('Values should be an array with one element for the first pixel of each band', async function() {
         expect(response.entity.values).toEqual( [100,200,300,400,500] );
+    });
+
+});
+
+describe("When a values POST request provides the dataset id both in the request body and on the query line", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/values?datasetId=not-a-dataset&variableName=temp&longitude=-123.0&latitude=45.0&start=0&end=4',
+		    entity: {
+		    	datasetId: '5x5x5'
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('Dataset id in response should match that in the request body', async function() {
+        expect(response.entity.datasetId).toBe('5x5x5');
+    });
+
+});
+
+describe("When a values POST request provides the variable name both in the request body and on the query line", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/values?datasetId=5x5x5&variableName=not-a-variable&longitude=-123.0&latitude=45.0&start=0&end=4',
+		    entity: {
+		    	variableName: 'temp'
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('Variable name in response should match that in the request body', async function() {
+        expect(response.entity.variableName).toBe('temp');
+    });
+
+});
+
+describe("When a values POST request provides the coordinates both in the request body and on the query line", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/values?datasetId=5x5x5&variableName=temp&longitude=0.0&latitude=0.0&start=0&end=4',
+		    entity: {
+		    	boundaryGeometry: {
+		    		type: 'Point',
+		    		coordinates: [-123, 45]
+		    	}
+	    	}
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('Boundary geometry coordinates should match those given in request body', async function() {
+        expect(response.entity.boundaryGeometry.coordinates).toEqual( [-123, 45] );
+    });
+});
+
+
+describe("When a values POST request provides the band range both in the request body and on the query line", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/values?datasetId=5x5x5&variableName=temp&longitude=-123.0&latitude=45.0&start=5&end=10',
+		    entity: {
+		    	start: 0,
+		    	end: 4
+	    	}
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
     });
 
 });
