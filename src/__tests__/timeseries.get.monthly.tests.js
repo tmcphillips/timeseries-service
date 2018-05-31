246ae7f5c08b0d2bfb2905839a8e0af42423838a
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-05-31 15:51:49 -0700

    Renamed 5x5x60 int16 dataset and updated tests.

--- src/__tests__/timeseries.get.monthly.tests.js
+++ src/__tests__/timeseries.get.monthly.tests.js
@@ -12,7 +12,7 @@ describe("When a timeseries GET request selects first pixel of each band in firs
 	beforeAll(async () => {
 		response = await callRESTService({
 		    method: 'GET',
-		    path: timeseriesServiceBase + '/timeseries/monthly/5x5x60_int16?longitude=-123.0&latitude=45.0&start=0001-01&end=0001-12&timeResolution=month&timeZero=0001-01'
+		    path: timeseriesServiceBase + '/timeseries/monthly_5x5x60_dataset/int16_variable?longitude=-123.0&latitude=45.0&start=0001-01&end=0001-12&timeResolution=month&timeZero=0001-01'
 		});
     });
 
@@ -21,11 +21,11 @@ describe("When a timeseries GET request selects first pixel of each band in firs
     });
 
     it ('Dataset id should match that in the request', async function() {
-        expect(response.entity.datasetId).toBe('monthly');
+        expect(response.entity.datasetId).toBe('monthly_5x5x60_dataset');
     });
 
 	it ('Variable name should match that in the request', async function() {
-        expect(response.entity.variableName).toBe('5x5x60_int16');
+        expect(response.entity.variableName).toBe('int16_variable');
     });
 
 	it ('Boundary geometry type should be point', async function() {
@@ -47,7 +47,7 @@ describe("When a timeseries GET request selects first pixel of each band in firs
     
     it ('Csv should comprise dates and values for each band for first 12 months', async function() {
         expect(response.entity.csv).toEqual( 
-        		"month, 5x5x60_int16"	+ "\n" +
+        		"month, int16_variable"	+ "\n" +
         		"0001-01, 100"			+ "\n" +
         		"0001-02, 200"			+ "\n" +
         		"0001-03, 300"			+ "\n" +
@@ -72,7 +72,7 @@ describe("When a timeseries GET request selects first pixel of each band in thir
 	beforeAll(async () => {
 		response = await callRESTService({
 		    method: 'GET',
-		    path: timeseriesServiceBase + '/timeseries/monthly/5x5x60_int16?longitude=-123.0&latitude=45.0&start=0003-01&end=0003-12&timeResolution=month&timeZero=0001-01'
+		    path: timeseriesServiceBase + '/timeseries/monthly_5x5x60_dataset/int16_variable?longitude=-123.0&latitude=45.0&start=0003-01&end=0003-12&timeResolution=month&timeZero=0001-01'
 		});
     });
 
@@ -91,7 +91,7 @@ describe("When a timeseries GET request selects first pixel of each band in thir
     
     it ('Csv should comprise dates and values for each month in third year', async function() {
         expect(response.entity.csv).toEqual( 
-        		"month, 5x5x60_int16"	+ "\n" +
+        		"month, int16_variable"	+ "\n" +
         		"0003-01, 2500"			+ "\n" +
         		"0003-02, 2600"			+ "\n" +
         		"0003-03, 2700"			+ "\n" +
@@ -106,124 +106,123 @@ describe("When a timeseries GET request selects first pixel of each band in thir
         		"0003-12, 3600"			+ "\n"
 		);
     });
+});
     
-    describe("When a timeseries GET request selects first pixel of each band in the 12 months starting at mid-year of year 4 of monthly 5x5x60 data cube", async () => {
-        
-    	var response;
-    	
-    	beforeAll(async () => {
-    		response = await callRESTService({
-    		    method: 'GET',
-    		    path: timeseriesServiceBase + '/timeseries/monthly/5x5x60_int16?longitude=-123.0&latitude=45.0&start=0004-07&end=0005-06&timeResolution=month&timeZero=0001-01'
-    		});
-        });
-
-        it ('HTTP response status code should be 200 - success', async function() {
-            expect(response.status.code).toBe(200);
-        });
-
-        it ('Series range start and end should match the request', async function() {
-            expect(response.entity.start).toEqual( "0004-07" );
-            expect(response.entity.end).toEqual( "0005-06" );
-        });
-        
-        it ('Values should be an array with one element for the first pixel for each of 12 months starting at mid-year of year 4', async function() {
-            expect(response.entity.values).toEqual( [4300,4400,4500,4600,4700,4800,4900,5000,5100,5200,5300,5400] );
-        });
-        
-        it ('Csv should comprise dates and values for 12 months starting at mid-year of year 4', async function() {
-            expect(response.entity.csv).toEqual( 
-            		"month, 5x5x60_int16"	+ "\n" +
-            		"0004-07, 4300"			+ "\n" +
-            		"0004-08, 4400"			+ "\n" +
-            		"0004-09, 4500"			+ "\n" +
-            		"0004-10, 4600"			+ "\n" +
-            		"0004-11, 4700"			+ "\n" +
-            		"0004-12, 4800"			+ "\n" +
-            		"0005-01, 4900"			+ "\n" +
-            		"0005-02, 5000"			+ "\n" +
-            		"0005-03, 5100"			+ "\n" +
-            		"0005-04, 5200"			+ "\n" +
-            		"0005-05, 5300"			+ "\n" +
-            		"0005-06, 5400"			+ "\n"
-    		);
-        });
-    });
+describe("When a timeseries GET request selects first pixel of each band in the 12 months starting at mid-year of year 4 of monthly 5x5x60 data cube", async () => {
     
-    describe("When a values GET request selects first pixel of each band in year starting at mid-year of last year of monthly 5x5x60 data cube", async () => {
-        
-    	var response;
-    	
-    	beforeAll(async () => {
-    		response = await callRESTService({
-    		    method: 'GET',
-    		    path: timeseriesServiceBase + '/timeseries/monthly/5x5x60_int16?longitude=-123.0&latitude=45.0&start=0005-07&end=0005-12&timeResolution=month&timeZero=0001-01'
-    		});
-        });
-
-        it ('HTTP response status code should be 200 - success', async function() {
-            expect(response.status.code).toBe(200);
-        });
-
-        it ('Series range start and end should match the request', async function() {
-            expect(response.entity.start).toEqual( "0005-07" );
-            expect(response.entity.end).toEqual( "0005-12" );
-        });
-        
-        it ('Values should be an array with one element for the first pixel of each month starting at mid-year of last year', async function() {
-            expect(response.entity.values).toEqual( [5500,5600,5700,5800,5900,6000] );
-        });
-        
-        it ('Csv should comprise dates and values for each month starting at mid-year of last year of monthly 5x5x60 data cube', async function() {
-            expect(response.entity.csv).toEqual( 
-            		"month, 5x5x60_int16"	+ "\n" +
-            		"0005-07, 5500"			+ "\n" +
-            		"0005-08, 5600"			+ "\n" +
-            		"0005-09, 5700"			+ "\n" +
-            		"0005-10, 5800"			+ "\n" +
-            		"0005-11, 5900"			+ "\n" +
-            		"0005-12, 6000"			+ "\n"
-    		);
-        });
-    });
-    
-    describe("When a values GET request selects first pixel of each band in first year of monthly 5x5x60 data cube with an 18-month time zero offset", async () => {
-        
-    	var response;
-    	
-    	beforeAll(async () => {
-    		response = await callRESTService({
-    		    method: 'GET',
-    		    path: timeseriesServiceBase + '/timeseries/monthly/5x5x60_int16?longitude=-123.0&latitude=45.0&start=0002-07&end=0002-12&timeResolution=month&timeZero=0002-07'
-    		});
-        });
-
-        it ('HTTP response status code should be 200 - success', async function() {
-            expect(response.status.code).toBe(200);
-        });
-
-        it ('Series range start and end should match the request', async function() {
-            expect(response.entity.start).toEqual( "0002-07" );
-            expect(response.entity.end).toEqual( "0002-12" );
-        });
-        
-        it ('Values should be an array with one element for the first pixel of each of the first six months in the data set', async function() {
-            expect(response.entity.values).toEqual( [100,200,300,400,500,600] );
-        });
-        
-        it ('Csv should comprise dates and values for each of the first six months in the data set', async function() {
-            expect(response.entity.csv).toEqual( 
-            		"month, 5x5x60_int16"	+ "\n" +
-            		"0002-07, 100"			+ "\n" +
-            		"0002-08, 200"			+ "\n" +
-            		"0002-09, 300"			+ "\n" +
-            		"0002-10, 400"			+ "\n" +
-            		"0002-11, 500"			+ "\n" +
-            		"0002-12, 600"			+ "\n"
-    		);
-        });
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'GET',
+		    path: timeseriesServiceBase + '/timeseries/monthly_5x5x60_dataset/int16_variable?longitude=-123.0&latitude=45.0&start=0004-07&end=0005-06&timeResolution=month&timeZero=0001-01'
+		});
     });
 
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('Series range start and end should match the request', async function() {
+        expect(response.entity.start).toEqual( "0004-07" );
+        expect(response.entity.end).toEqual( "0005-06" );
+    });
+    
+    it ('Values should be an array with one element for the first pixel for each of 12 months starting at mid-year of year 4', async function() {
+        expect(response.entity.values).toEqual( [4300,4400,4500,4600,4700,4800,4900,5000,5100,5200,5300,5400] );
+    });
+    
+    it ('Csv should comprise dates and values for 12 months starting at mid-year of year 4', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"month, int16_variable"	+ "\n" +
+        		"0004-07, 4300"			+ "\n" +
+        		"0004-08, 4400"			+ "\n" +
+        		"0004-09, 4500"			+ "\n" +
+        		"0004-10, 4600"			+ "\n" +
+        		"0004-11, 4700"			+ "\n" +
+        		"0004-12, 4800"			+ "\n" +
+        		"0005-01, 4900"			+ "\n" +
+        		"0005-02, 5000"			+ "\n" +
+        		"0005-03, 5100"			+ "\n" +
+        		"0005-04, 5200"			+ "\n" +
+        		"0005-05, 5300"			+ "\n" +
+        		"0005-06, 5400"			+ "\n"
+		);
+    });
+});
+
+describe("When a values GET request selects first pixel of each band in year starting at mid-year of last year of monthly 5x5x60 data cube", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'GET',
+		    path: timeseriesServiceBase + '/timeseries/monthly_5x5x60_dataset/int16_variable?longitude=-123.0&latitude=45.0&start=0005-07&end=0005-12&timeResolution=month&timeZero=0001-01'
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('Series range start and end should match the request', async function() {
+        expect(response.entity.start).toEqual( "0005-07" );
+        expect(response.entity.end).toEqual( "0005-12" );
+    });
+    
+    it ('Values should be an array with one element for the first pixel of each month starting at mid-year of last year', async function() {
+        expect(response.entity.values).toEqual( [5500,5600,5700,5800,5900,6000] );
+    });
+    
+    it ('Csv should comprise dates and values for each month starting at mid-year of last year of monthly 5x5x60 data cube', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"month, int16_variable"	+ "\n" +
+        		"0005-07, 5500"			+ "\n" +
+        		"0005-08, 5600"			+ "\n" +
+        		"0005-09, 5700"			+ "\n" +
+        		"0005-10, 5800"			+ "\n" +
+        		"0005-11, 5900"			+ "\n" +
+        		"0005-12, 6000"			+ "\n"
+		);
+    });
+});
+
+describe("When a values GET request selects first pixel of each band in first year of monthly 5x5x60 data cube with an 18-month time zero offset", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'GET',
+		    path: timeseriesServiceBase + '/timeseries/monthly_5x5x60_dataset/int16_variable?longitude=-123.0&latitude=45.0&start=0002-07&end=0002-12&timeResolution=month&timeZero=0002-07'
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+
+    it ('Series range start and end should match the request', async function() {
+        expect(response.entity.start).toEqual( "0002-07" );
+        expect(response.entity.end).toEqual( "0002-12" );
+    });
+    
+    it ('Values should be an array with one element for the first pixel of each of the first six months in the data set', async function() {
+        expect(response.entity.values).toEqual( [100,200,300,400,500,600] );
+    });
+    
+    it ('Csv should comprise dates and values for each of the first six months in the data set', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"month, int16_variable"	+ "\n" +
+        		"0002-07, 100"			+ "\n" +
+        		"0002-08, 200"			+ "\n" +
+        		"0002-09, 300"			+ "\n" +
+        		"0002-10, 400"			+ "\n" +
+        		"0002-11, 500"			+ "\n" +
+        		"0002-12, 600"			+ "\n"
+		);
+    });
 });
 
 describe("When a values GET request selects first pixel of each band in year starting at mid-year of last year of monthly 5x5x60 data cube with an 18-month time zero offset", async () => {
@@ -233,7 +232,7 @@ describe("When a values GET request selects first pixel of each band in year sta
 	beforeAll(async () => {
 		response = await callRESTService({
 		    method: 'GET',
-		    path: timeseriesServiceBase + '/timeseries/monthly/5x5x60_int16?longitude=-123.0&latitude=45.0&start=0007-01&end=0007-06&timeResolution=month&timeZero=0002-07'
+		    path: timeseriesServiceBase + '/timeseries/monthly_5x5x60_dataset/int16_variable?longitude=-123.0&latitude=45.0&start=0007-01&end=0007-06&timeResolution=month&timeZero=0002-07'
 		});
     });
 
@@ -252,7 +251,7 @@ describe("When a values GET request selects first pixel of each band in year sta
     
     it ('Csv should comprise dates and values for each of the last six months in the data set', async function() {
         expect(response.entity.csv).toEqual( 
-        		"month, 5x5x60_int16"	+ "\n" +
+        		"month, int16_variable"	+ "\n" +
         		"0007-01, 5500"			+ "\n" +
         		"0007-02, 5600"			+ "\n" +
         		"0007-03, 5700"			+ "\n" +
