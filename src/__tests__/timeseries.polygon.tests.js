f1765fb7ad98ee0d464507452dea60f168eb2c71
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-06-26 23:12:04 -0700

    Tests exercising area timeseries with NaN nodata.

--- src/__tests__/timeseries.polygon.tests.js
+++ src/__tests__/timeseries.polygon.tests.js
@@ -5,7 +5,58 @@ const timeseriesServiceBase = 'http://localhost:8001/timeseries-service/api/v1';
 
 const callRESTService =  rest.wrap(mime, { mime: 'application/json' } );
 
-describe("When a GET request selects from a region exactly covering a 2x2 pixel area in the dataset", async () => {
+describe("When a GET request selects from a region exactly covering the northwest 2x2 pixel area in the uint16 variable", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'uint16_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+	    		    	[-123,45],
+	    		        [-123,47],
+	    		        [-121,47],
+	    		        [-121,45],
+	    		        [-123,45]
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+    
+    it ('The array should comprise the average of four pixels in each band', async function() {
+        expect(response.entity.values[0]).toBeCloseTo(105.5,3);
+        expect(response.entity.values[1]).toBeCloseTo(205.5,3);
+        expect(response.entity.values[2]).toBeCloseTo(305.5,3);
+        expect(response.entity.values[3]).toBeCloseTo(405.5,3);
+        expect(response.entity.values[4]).toBeCloseTo(505.5,3);
+    });
+    
+    it ('The data column should comprise the average of four pixels in each band', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"index, uint16_variable"	+ "\n" +
+        		"0,105.500"					+ "\n" +
+        		"1,205.500"					+ "\n" +
+        		"2,305.500"					+ "\n" +
+        		"3,405.500" 				+ "\n" +
+        		"4,505.500" 				+ "\n"
+		);
+    });
+});
+
+describe("When a GET request selects from a region exactly covering the northwest 2x2 pixel area in the float32 variable", async () => {
     
 	var response;
 	
@@ -56,7 +107,7 @@ describe("When a GET request selects from a region exactly covering a 2x2 pixel 
     });
 });
 
-describe("When a GET request selects from a region of zero area using a polygon geometry type", async () => {
+describe("When a GET request selects from a region of zero area from the uint16 variable", async () => {
     
 	var response;
 	
@@ -96,7 +147,48 @@ describe("When a GET request selects from a region of zero area using a polygon 
     }); 
 });
 
-describe("When a GET request selects from a 2x2 pixel region that intersects a 2x1 pixel region in dataset", async () => {
+describe("When a GET request selects from a region of zero area from the float32 variable", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'float32_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+	    		    	[-122.5,45.5],
+	    		    	[-122.5,45.5],
+	    		    	[-122.5,45.5],
+	    		    	[-122.5,45.5],
+	    		    	[-122.5,45.5],
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 400', async function() {
+        expect(response.status.code).toBe(400);
+    });
+    
+    it ('Error summary should be bad request', async function() {
+        expect(response.entity.error).toBe("Bad Request");
+    });
+
+    it ('Error message should be that longitude parameter is not present', async function() {
+        expect(response.entity.message).toContain("Input shapes do not overlap raster.")
+    }); 
+});
+
+
+describe("When a GET request selects from a 2x2 pixel region that intersects a 2x1 pixel region in the uint16 variable", async () => {
     
 	var response;
 	
@@ -147,7 +239,59 @@ describe("When a GET request selects from a 2x2 pixel region that intersects a 2
     });
 });
 
-describe("When a GET request selects a region just outside coverage of dataset", async () => {
+describe("When a GET request selects from a 2x2 pixel region that intersects a 2x1 pixel region in the float32 variable", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'float32_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+    		    	[-123,44],
+    		        [-123,46],
+    		        [-121,46],
+    		        [-121,44],
+    		        [-123,44]
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+    
+    it ('The array should comprise the average of four pixels in each band', async function() {
+        expect(response.entity.values[0]).toBeCloseTo(100.55,3);
+        expect(response.entity.values[1]).toBeCloseTo(200.55,3);
+        expect(response.entity.values[2]).toBeCloseTo(300.55,3);
+        expect(response.entity.values[3]).toBeCloseTo(400.55,3);
+        expect(response.entity.values[4]).toBeCloseTo(500.55,3);
+    });
+    
+    it ('The data column should comprise the average of the two pixels intersection of polygon and dataset', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"index, float32_variable, range -, range +" + "\n" +  
+        		"0,100.550,90.5000,110.600"					+ "\n" +
+        		"1,200.550,180.500,220.600"					+ "\n" +
+        		"2,300.550,270.500,330.600"					+ "\n" +
+        		"3,400.550,360.500,440.600"					+ "\n" +
+        		"4,500.550,450.500,550.600"					+ "\n"
+		);
+    });
+});
+
+
+describe("When a GET request selects a region just outside coverage of the uint16 variable", async () => {
     
 	var response;
 	
@@ -187,7 +331,48 @@ describe("When a GET request selects a region just outside coverage of dataset",
     }); 
 });
 
-describe("When a GET request selects exactly the top-left corner pixel", async () => {
+describe("When a GET request selects a region just outside coverage of the float32 variable", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'float32_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+    		    	[-124,45],
+    		        [-124,47],
+    		        [-123,47],
+    		        [-123,45],
+    		        [-123,45]
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 400', async function() {
+        expect(response.status.code).toBe(400);
+    });
+    
+    it ('Error summary should be bad request', async function() {
+        expect(response.entity.error).toBe("Bad Request");
+    });
+
+    it ('Error message should be that longitude parameter is not present', async function() {
+        expect(response.entity.message).toContain("Input shapes do not overlap raster.")
+    }); 
+});
+
+
+describe("When a GET request selects exactly the top-left corner pixel of the uint16 variable", async () => {
     
 	var response;
 	
@@ -238,7 +423,59 @@ describe("When a GET request selects exactly the top-left corner pixel", async (
     });
 });
 
-describe("When a GET request selects 1/4 of the top-left corner pixel", async () => {
+describe("When a GET request selects exactly the top-left corner pixel of the float32 variable", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'float32_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+	    		    	[-123.0,45],
+	    		        [-123.0,46],
+	    		        [-122,46],
+	    		        [-122,45],
+	    		        [-123.0,45]
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+    
+    it ('The array should comprise the values of the top-left pixels in each band', async function() {
+        expect(response.entity.values[0]).toBeCloseTo(100.0,3);
+        expect(response.entity.values[1]).toBeCloseTo(200.0,3);
+        expect(response.entity.values[2]).toBeCloseTo(300.0,3);
+        expect(response.entity.values[3]).toBeCloseTo(400.0,3);
+        expect(response.entity.values[4]).toBeCloseTo(500.0,3);
+    });
+    
+    it ('The data column should comprise the values of the top-left pixels in each band', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"index, float32_variable, range -, range +" + "\n" +
+        		"0,100.000,90.0000,110.000" 				+ "\n" +
+        		"1,200.000,180.000,220.000" 				+ "\n" +
+        		"2,300.000,270.000,330.000" 				+ "\n" +
+        		"3,400.000,360.000,440.000" 				+ "\n" +
+        		"4,500.000,450.000,550.000"					+ "\n"
+		);
+    });
+});
+
+
+describe("When a GET request selects 1/4 of the top-left corner pixel of the uint16 variable", async () => {
     
 	var response;
 	
@@ -288,6 +525,58 @@ describe("When a GET request selects 1/4 of the top-left corner pixel", async ()
 		);
     });
 });
+
+describe("When a GET request selects 1/4 of the top-left corner pixel of the float32 variable", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'float32_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+	    		    	[-123.0,45],
+	    		        [-123.0,45.25],
+	    		        [-122.75,45.25],
+	    		        [-122.75,45.25],
+	    		        [-123.0,45]
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+    
+    it ('The array should comprise the values of the top-left pixels in each band', async function() {
+        expect(response.entity.values[0]).toBeCloseTo(100.0,3);
+        expect(response.entity.values[1]).toBeCloseTo(200.0,3);
+        expect(response.entity.values[2]).toBeCloseTo(300.0,3);
+        expect(response.entity.values[3]).toBeCloseTo(400.0,3);
+        expect(response.entity.values[4]).toBeCloseTo(500.0,3);
+    });
+    
+    it ('The data column should comprise the values of the top-left pixels in each band', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"index, float32_variable, range -, range +" + "\n" +
+        		"0,100.000,90.0000,110.000" 				+ "\n" +
+        		"1,200.000,180.000,220.000" 				+ "\n" +
+        		"2,300.000,270.000,330.000" 				+ "\n" +
+        		"3,400.000,360.000,440.000" 				+ "\n" +
+        		"4,500.000,450.000,550.000"					+ "\n"
+		);
+    });
+});
+
 
 
 describe("When a GET request selects from a region exactly covering the 2x2 pixel area in southwest corner the dataset", async () => {
@@ -341,7 +630,7 @@ describe("When a GET request selects from a region exactly covering the 2x2 pixe
     });
 });
 
-describe("When a GET request selects from a region exactly covering the 2x2 pixel area in southeast corner of dataset with one NODATA pixel in each band", async () => {
+describe("When a GET request selects from a region exactly covering the 2x2 pixel area in southeast corner of uint16 variable with one NODATA pixel in each band", async () => {
     
 	var response;
 	
@@ -392,7 +681,55 @@ describe("When a GET request selects from a region exactly covering the 2x2 pixe
     });
 });
 
-describe("When a GET request selects from a region exactly covering the 2x2 pixel area in southeast corner of dataset with two NODATA pixels in 3rd band", async () => {
+describe("When a GET request selects from a region exactly covering the 2x2 pixel area in southeast corner of float32 variable with one NODATA pixel in each band", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'float32_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+	    		    	[-120,48],
+	    		        [-118,50],
+	    		        [-120,50],
+	    		        [-120,48],
+	    		        [-118,48]
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+    
+    it ('The array should comprise the average of four pixels in each band', async function() {
+        expect(response.entity.values).toEqual([null, null, null, null, null]);
+    });
+    
+    it ('The data column should comprise the average of four pixels in each band', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"index, float32_variable, range -, range +"	+ "\n" +
+        		"0,,,"				    					+ "\n" +
+        		"1,,,"				    					+ "\n" +
+        		"2,,,"				    					+ "\n" +
+        		"3,,,"				    					+ "\n" +
+    			"4,,,"				    					+ "\n"
+		);
+    });
+});
+
+
+describe("When a GET request selects from a region exactly covering the 2x2 pixel area in uint16 variable with one NODATA pixel in 3rd band", async () => {
     
 	var response;
 	
@@ -443,4 +780,55 @@ describe("When a GET request selects from a region exactly covering the 2x2 pixe
     });
 });
 
+
+describe("When a GET request selects from a region exactly covering the 2x2 pixel area in southeast corner of float32 variable with two NODATA pixels in 3rd band", async () => {
+    
+	var response;
+	
+	beforeAll(async () => {
+		response = await callRESTService({
+		    method: 'POST',
+		    path: timeseriesServiceBase + '/timeseries',
+		    entity: {
+		    	datasetId: 'annual_5x5x5_dataset',
+		    	variableName: 'float32_variable',
+		    	boundaryGeometry: {
+    		    "type": "Polygon",
+    		    "coordinates": [[
+	    		    	[-120,46],
+	    		        [-118,48],
+	    		        [-120,48],
+	    		        [-120,46],
+	    		        [-118,46]
+	    		    	]]
+	    		},
+	    		start: 0,
+	    		end: 4
+		    }
+		});
+    });
+
+    it ('HTTP response status code should be 200 - success', async function() {
+        expect(response.status.code).toBe(200);
+    });
+    
+    it ('The array should comprise the average of four pixels in each band', async function() {
+        expect(response.entity.values[0]).toBeCloseTo(118.850,3);
+        expect(response.entity.values[1]).toBeCloseTo(218.850,3);
+        expect(response.entity.values[2]).toBe(null);
+        expect(response.entity.values[3]).toBeCloseTo(418.850,3);
+        expect(response.entity.values[4]).toBeCloseTo(518.850,3);
+    });
+    
+    it ('The data column should comprise the average of four pixels in each band', async function() {
+        expect(response.entity.csv).toEqual( 
+        		"index, float32_variable, range -, range +"	+ "\n" +
+        		"0,118.850,107.000,130.700"					+ "\n" +
+        		"1,218.850,197.000,240.700"					+ "\n" +
+        		"2,,,"										+ "\n" +
+        		"3,418.850,377.000,460.700"					+ "\n" +
+        		"4,518.850,467.000,570.700"	 				+ "\n"
+		);
+    });
+});
 
