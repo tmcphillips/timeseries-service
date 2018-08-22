b4416fc69c2cd42e5c2949596829cc49d9a13bf6
Timothy McPhillips <tmcphillips@absoluteflow.org>
2018-08-22 15:31:28 -0700

    add GDAL to package dependencies

--- python/skope_analysis/setup.py
+++ python/skope_analysis/setup.py
@@ -12,5 +12,8 @@ setup(
     packages=['skope_analysis'],
     package_dir={'': 'src'},
     data_files = [("", ["LICENSE.txt"])],
-    install_requires=['Flask >= 1.0.2']
+    install_requires=[
+        'Flask >= 1.0.2',
+        'GDAL >= 2.3.1'
+    ]
 )
